27d73d2c88c5ee340ad75dc990cf9fabc3d5e3a5
omarfsumon <hmfarukhossain96@gmail.com>
2025-09-13 19:05:02 +0600

    Updat fron page some section

--- front-page.php
+++ front-page.php
@@ -294,15 +294,15 @@
     <div class="container">
         <div class="flex flex-wrap justify-center">
             <div class="lg:w-1/2 text-center">
-                <span class="subtitle text-secondary-900 text-sm font-normal uppercase tracking-wider py-1 px-2 self-center bg-secondary-100  fadeInUp animated" data--delay=".0s">Dicover Gallery</span>
-                <h2 class=" text-2nd-heading  fadeInUp animated" data--delay=".2s">Exterior &amp; Interior</h2>
+                <span class="subtitle text-secondary-900 text-sm font-normal uppercase tracking-wider py-1 px-2 self-center bg-secondary-100  fadeInUp animated" data--delay=".0s">What We Dd</span>
+                <h2 class=" text-2nd-heading  fadeInUp animated" data--delay=".2s">Our Services</h2>
             </div>
         </div>
 
         <div class="flex w-full text-center justify-center items-center mb-12 mt-10">
             <ul id="filters" class=" fadeInUp animated flex flex-wrap justify-center lg:flex-row" data--delay="0s">
-                <li><a href="#" data-filter="*" class="selected">View All</a></li>
-                <li><a href="#" data-filter=".exterior">Exterior</a></li>
+                <li><a href="#" data-filter="*" class="selected">Joint Venture</a></li>
+                <li><a href="#" data-filter=".buyers">Buyers</a></li>
                 <li><a href="#" data-filter=".interior">Interior</a></li>
                 <li><a href="#" data-filter=".facilities">Facilities</a></li>
             </ul>
@@ -457,27 +457,95 @@
     </div>
 </section>
 <section id="why-innovera" class="bg-[#003333] py-24 text-white">
-    <div class="container flex flex-col lg:flex-row gap-5 items-center justify-center">
+    <div class="container flex flex-col lg:flex-row gap-5 items-center justify-center lg:justify-between">
         <div class="w-full lg:w-1/2">
             <span class="subtitle  fadeInUp animated text-secondary-100 text-sm font-normal uppercase tracking-wider py-1 px-2 bg-[#1E4645] rounded" data--delay=".2s">Discover</span>
             <h2 class=" fadeInUp animated text-2nd-heading text-white" data--delay=".4s">Why Choose Innovera?</h2>
             <p class=" fadeInUp animated" data--delay=".6s">
                 At Innovera Holdings Ltd., we don’t just build apartments—we create lasting value and lifestyles. Here’s why customers and landowners trust us with their biggest investments:
             </p>
-            <div class="icon-list">
+            <div class="icon-list mt-3">
                 <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
-                    <div class="text-3xl text-secondary-400 mr-4 mt-1">
-                        <i class="fas fa-check-circle"></i>
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="M468.08-162.31q7.84 0 16.07-3.54 8.23-3.53 13.31-8.61l311.85-311.85q15.07-15.07 23.27-30.84 8.19-15.77 8.19-34.62 0-19.08-8.19-37.04-8.2-17.96-23.27-32.81l-150-150q-14.85-15.07-30.5-22.11-15.66-7.04-34.73-7.04-18.85 0-35 7.04-16.16 7.04-30.46 22.11l-34.85 34.85 74 74.23q11.92 11.69 17.77 26.62 5.84 14.92 5.84 30.3 0 31.24-20.42 51.66-20.42 20.42-51.65 20.42-15.39 0-30.04-4.69-14.65-4.69-26.35-16.39l-76.54-76.3-172.69 172.69q-6.07 6.08-9.11 13.81-3.04 7.73-3.04 15.57 0 14.16 9.08 23.35 9.07 9.19 23.23 9.19 7.84 0 16.07-3.54 8.23-3.53 13.31-8.61L390-535.23l28.31 28.31-126.54 126.77q-6.08 6.07-9.12 13.8-3.03 7.73-3.03 15.58 0 13.39 9.46 22.85t22.84 9.46q7.85 0 16.08-3.54 8.23-3.54 13.31-8.62l136-135.76 28.31 28.3-135.77 136q-5.31 5.08-8.73 13.31-3.43 8.23-3.43 16.08 0 13.38 9.46 22.84 9.47 9.47 22.85 9.47 7.85 0 15.58-3.04t13.8-9.12l136-135.77L583.69-360l-136 136q-6.07 6.08-9.11 14.58-3.04 8.5-3.04 15.57 0 14.16 9.96 22.85 9.96 8.69 22.58 8.69Zm-.23 40q-30.85 0-52.81-22.57-21.96-22.58-19.27-55.74-34 .39-56.62-20.3-22.61-20.7-21.46-57.77-37.07.38-58.42-21.19-21.35-21.58-19.42-56.89-33.39.39-55.85-18.77-22.46-19.15-22.46-53.31 0-15.38 5.96-30.8 5.96-15.43 17.65-27.12l201.23-201 103.31 103.31q5.08 5.31 12.54 8.73 7.46 3.42 16.85 3.42 12.84 0 22.69-8.96 9.85-8.96 9.85-23.35 0-9.38-3.43-16.84-3.42-7.46-8.73-12.54L401.85-771.62q-14.85-15.07-30.89-22.11-16.04-7.04-35.11-7.04-18.85 0-34.23 7.04-15.39 7.04-30.47 22.11l-121.77 122q-12.84 12.85-20.76 30.62-7.93 17.77-8.39 36.69-.46 13.54 2.31 26.04t8.77 23.5L101-502.46q-10.08-16.08-15.77-37.04-5.69-20.96-5.23-42.81.46-27.23 10.92-52.19 10.46-24.96 29.93-44.42l121-121q20.92-20.7 44.27-30.77 23.34-10.08 50.5-10.08 27.15 0 50.11 10.08 22.96 10.07 43.65 30.77l34.85 34.84 34.85-34.84q20.92-20.7 43.88-30.77 22.96-10.08 50.12-10.08 27.15 0 50.5 10.08 23.34 10.07 44.04 30.77l149 149q20.69 20.69 31.92 46.46t11.23 52.92q0 27.16-11.23 50.12-11.23 22.96-31.92 43.65L525.77-146.15q-12.46 12.46-27.12 18.15-14.65 5.69-30.8 5.69ZM339.62-626.92Z"/></svg>
+                        </i>
                     </div>
                     <div>
-                        <h4 class="text-xl font-semibold mb-1">Trust & Transparency</h4>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Trust & Transparency</h4>
                         <p class="mb-0 opacity-60">We believe in honest communication, fair contracts, and no hidden costs. From booking to handover, our process is fully transparent.</p>
+                    </div>
+                </div>
+                <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                        </i>
+                    </div>
+                    <div>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Quality Construction</h4>
+                        <p class="mb-0 opacity-60">We use top-grade materials, modern engineering techniques, and strict quality control to ensure that your home stands strong for generations.</p>
+                    </div>
+                </div>
+                <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/></svg>
+                        </i>
+                    </div>
+                    <div>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Customization Options</h4>
+                        <p class="mb-0 opacity-60">Your home should reflect your lifestyle. With our Interior & Customization service, you can choose tiles, fittings, finishes, and layouts to make your space truly yours.</p>
+                    </div>
+                </div>
+                <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="M20-248.46v-48.77q0-39.92 41.69-65.58 41.7-25.65 108.7-25.65 11.07 0 22.3.69t22.62 2.69q-11.69 18.7-17.35 38.81-5.65 20.12-5.65 41.27v56.54H20Zm240 0v-55q0-28.09 15.77-51.35 15.77-23.27 45.46-40.57 29.69-17.31 70.16-25.96 40.46-8.66 88.46-8.66 48.92 0 89.38 8.66 40.46 8.65 70.15 25.96 29.7 17.3 45.16 40.57Q700-331.55 700-303.46v55H260Zm507.69 0v-56.42q0-22.57-5.34-42.54-5.35-19.96-16.04-37.66 11.77-2 22.5-2.69t21.19-.69q67 0 108.5 25.35 41.5 25.34 41.5 65.88v48.77H767.69Zm-444.61-60h314.46v-4.23q-6.15-24.23-51.08-40.77Q541.54-370 480-370q-61.54 0-106.46 16.54-44.93 16.54-50.46 40.77v4.23Zm-152.8-118.85q-28.28 0-48.32-20.11t-20.04-48.35q0-28.61 20.12-48.34 20.11-19.73 48.35-19.73 28.61 0 48.53 19.73 19.93 19.73 19.93 48.46 0 27.88-19.72 48.11-19.71 20.23-48.85 20.23Zm619.72 0q-28 0-48.23-20.23-20.23-20.23-20.23-48.11 0-28.73 20.23-48.46t48.3-19.73q28.93 0 48.66 19.73 19.73 19.73 19.73 48.34 0 28.24-19.68 48.35-19.68 20.11-48.78 20.11ZM480.14-460q-43.22 0-73.6-30.29-30.38-30.28-30.38-73.55 0-44.14 30.28-73.99 30.29-29.86 73.56-29.86 44.13 0 73.99 29.82 29.85 29.81 29.85 73.89 0 43.21-29.81 73.6Q524.21-460 480.14-460Zm.05-60q18.35 0 31-12.84 12.66-12.85 12.66-31.2 0-18.34-12.61-31-12.61-12.65-31.24-12.65-18.15 0-31 12.61-12.85 12.6-12.85 31.24 0 18.15 12.85 31Q461.85-520 480.19-520Zm.43 211.54ZM480-563.84Z"/></svg>
+                        </i>
+                    </div>
+                    <div>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Expert Team</h4>
+                        <p class="mb-0 opacity-60">From architects and engineers to customer service executives, our experienced professionals are committed to delivering excellence in every project.</p>
+                    </div>
+                </div>
+                <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="M480-181.54q-54.33 0-102.36-17.88-48.02-17.89-87.1-49.89l-69.46 68.46q-8.18 8.31-20.82 8.31t-20.95-8.31q-8.31-8.3-8.31-20.95 0-12.64 8.31-20.81l68.46-68.47q-32-39.07-49.88-87.59Q180-427.2 180-481.54q0-125.62 87.19-212.81T480-781.54h300v300q0 125.63-87.19 212.81-87.19 87.19-212.81 87.19Zm0-60q100 0 170-70t70-170v-240H480q-100 0-170 70t-70 170q0 41.69 13.54 79.31 13.54 37.62 37.61 67.77l208.16-208.15q8.18-8.31 20.82-8.31t21.06 8.42q9.19 9.19 9.19 21.27t-9.3 21.38L332.92-292.08q30.16 24.08 67.77 37.31 37.62 13.23 79.31 13.23Zm0-240Z"/></svg>
+                    </i>
+                    </div>
+                    <div>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Sustainability & Innovation</h4>
+                        <p class="mb-0 opacity-60">We care about the future. Our projects incorporate eco-friendly designs, green spaces, and energy-efficient features for sustainable living.</p>
+                    </div>
+                </div>
+                <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="M471.54-161.15q5.92 0 12.04-2.77 6.11-2.77 9.65-6.31l319.92-319.92q13.54-13.54 20.39-28.93 6.84-15.38 6.84-32.3 0-17.54-6.84-33.77-6.85-16.24-20.39-29.16l-160-160q-12.92-13.54-28-19.81-15.07-6.26-32.61-6.26-16.92 0-32.5 6.26-15.58 6.27-28.73 19.81l-22.93 22.93 74 74.61q13.46 12.85 19.89 29.31 6.42 16.46 6.42 34.15 0 36.62-24.46 61.08t-61.08 24.46q-17.69 0-34.27-5.85-16.57-5.84-29.42-18.69l-75.77-75.15-173.84 173.84q-4.54 4.54-6.81 10.16-2.27 5.61-2.27 11.54 0 11.07 7.54 18.92 7.54 7.85 18.61 7.85 5.93 0 12.04-2.77 6.12-2.77 9.66-6.31L380-545.61l42.15 42.15-130.77 131.38q-4.53 4.54-6.8 10.16-2.27 5.61-2.27 11.54 0 10.69 7.73 18.42 7.73 7.73 18.42 7.73 5.93 0 12.04-2.77 6.12-2.77 9.65-6.31l136-135.38 42.16 42.15-135.39 136q-4.15 3.54-6.61 9.65-2.46 6.12-2.46 12.04 0 10.7 7.73 18.43t18.42 7.73q5.92 0 11.54-2.27 5.61-2.27 10.15-6.81l136-135.38L589.85-345l-136 136q-4.54 4.54-6.81 10.54-2.27 6-2.27 11.54 0 11.07 8.23 18.42 8.23 7.35 18.54 7.35Zm-.62 59.99q-33.92 0-59.15-23.53-25.23-23.54-26.38-58.62-34-2.31-56.81-24.15-22.81-21.85-24.73-57.39-35.54-2.3-57.46-24.84-21.93-22.54-23.47-56.7-35.69-2.3-58.92-25.88-23.23-23.58-23.23-59.65 0-17.69 6.73-34.66 6.73-16.96 19.58-29.8l216.61-216 117.16 117.15q3.53 4.15 9.26 6.62 5.74 2.46 12.43 2.46 10.92 0 18.84-7.23 7.93-7.23 7.93-18.93 0-6.69-2.46-12.42-2.47-5.73-6.62-9.27L399.92-774.31q-12.92-13.54-28.19-19.81-15.27-6.26-32.81-6.26-16.92 0-32.11 6.26-15.2 6.27-28.73 19.81l-131.39 132q-10.92 10.92-17.88 25.81-6.96 14.88-8.19 30.35-1.24 12.77 1.15 25.27 2.38 12.5 8.38 23.5L86-493.23Q72.46-512.77 65.62-537q-6.85-24.23-5.62-49.15 1.23-27.62 12.46-53.35t31.46-45.96l131-131q22.47-21.85 48.89-32.88 26.42-11.04 55.5-11.04 29.07 0 55.3 11.04 26.24 11.03 48.08 32.88l22.93 22.92 22.92-22.92q22.46-21.85 48.69-32.88 26.23-11.04 55.31-11.04 29.08 0 55.5 11.04 26.42 11.03 48.27 32.88l159 159q21.84 21.85 33.46 49.73 11.61 27.88 11.61 56.96 0 29.08-11.61 55.31-11.62 26.23-33.46 48.07L535.38-128.08q-13.23 13.23-29.8 20.08-16.58 6.84-34.66 6.84Zm-113.61-532.3Z"/></svg>
+                        </i>
+                    </div>
+                    <div>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Customer-Centric Approach</h4>
+                        <p class="mb-0 opacity-60">Our relationship doesn’t end at handover. We offer after-sales support, property management, and lifelong assistance to keep your investment safe.</p>
+                    </div>
+                </div>
+                <div class="flex items-start mb-4 fadeInUp animated" data--delay=".8s">
+                    <div class="mr-3">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#fdf6ef">
+                            <path d="M480-100q-147.06 0-255.22-97.04T102-440h61q14.62 121 105.23 200.5Q358.85-160 480-160q134 0 227-93t93-227q0-134-93-227t-227-93q-90.23 0-166.04 46.35-75.81 46.34-116.88 127.11h118.3v60H110.31q30.15-129.23 133.42-211.34Q347-860 480-860q78.85 0 148.2 29.92t120.65 81.21q51.3 51.29 81.22 120.63Q860-558.9 860-480.07q0 78.84-29.93 148.21-29.92 69.37-81.22 120.68T628.2-129.93Q558.85-100 480-100Zm108.92-228.92L450-467.9v-197.48h60v173.23l120.69 121.07-41.77 42.16Z"/></svg>
+                        </i>
+                    </div>
+                    <div>
+                        <h4 class="text-xl font-normal mb-1 text-secondary-50">Proven Track Record</h4>
+                        <p class="mb-0 opacity-60">With multiple successful projects completed and ongoing developments across Dhaka, Innovera has earned a reputation for on-time delivery and unmatched reliability.</p>
                     </div>
                 </div>
             </div>
         </div>
         <div class="w-full lg:w-1/2">
-            <img width="720px" src="<?php echo INNOVERA_URI . '/src/images/misc/s4.webp' ?>" class="aspect-square object-cover rounded-2xl  fadeInUp animated" data--delay=".8s" alt="Floorplan">
+            <img width="720px" src="<?php echo INNOVERA_URI . '/src/images/apartment/why_innovera.webp' ?>" class="object-cover rounded-2xl  fadeInUp animated" data--delay=".8s" alt="Floorplan">
         </div>
     </div>
 </section>
