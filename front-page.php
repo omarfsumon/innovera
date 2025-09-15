e0f730c673282b897cc7c1f7532cfc0073be64de
omarfsumon <hmfarukhossain96@gmail.com>
2025-09-15 19:40:40 +0600

    home page section height solved

--- front-page.php
+++ front-page.php
@@ -1,10 +1,9 @@
 <?php get_header(); ?>
 
-<section id="hero" class="relative min-h-screen flex ">
+<section id="hero" class="relative lg:min-h-screen min-h-[75vh] flex ">
     <div class="z-20 flex items-center justify-center container">
         <div class="container mx-auto px-4">
             <div class="flex flex-col lg:flex-row gap-8 items-center justify-between mt-12">
-
                 <div class="w-full lg:w-6/12">
                     <h1 class="mb-0 text-hero-heading font-semibold leading-tight text-white  fadeInUp animated">
                         Trust - it's what we build.
@@ -428,7 +427,7 @@
 <section id="video" class="p-0">
     <div class="flex items-center w-full">
         <a class="block popup-youtube group w-full" href="https://www.youtube.com/watch?v=C6rf51uHWJg">
-            <div class="relative overflow-hidden h-[70vh]">
+            <div class="relative overflow-hidden h-[50vh] lg:h-[70vh]">
                 <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
                     <div class="w-20 h-20 rounded-full bg-white/5 hover:bg-orange-500 transition-all duration-300 flex items-center justify-center">
                         <svg xmlns="http://www.w3.org/2000/svg" height="80px" viewBox="0 -960 960 960" width="80px" fill="#FFFFFF">
@@ -453,16 +452,16 @@
         </div>
 
         <div class="flex md:flex-row flex-col justify-center items-center lg:gap-20 gap-10 px-5 lg:px-0">
-            <div class="lg:w-6/12">
+            <div class="flex lg:w-6/12 w-full fadeIn animated" data--delay=".4s">
+                <?php echo do_shortcode('[jet_fb_form form_id="57" submit_type="ajax" required_mark="*" fields_layout="column" fields_label_tag="label" markup_type="fieldset" enable_progress="" clear="1"]'); ?>
+            </div>
+            <div class="lg:w-6/12 md:order-first">
                 <h4 class="lg:text-4xl text-2xl font-normal text-black fadeInUp animated" data--delay=".4s">Call us directly and schedule a meeting right now!</h4>
                 <p class="lg:text-lg text-base font-normal text-black/50 fadeInUp animated">Our team is committed to honesty, professionalism, and delivering real value to our clients.</p>
                 <a class="flex items-center  gap-2 text-lg font-medium text-white hover:text-white px-3 py-2 mt-4 bg-secondary-500 hover:bg-secondary-600 rounded w-fit fadeInUp animated" href="tel:+8801833156010">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" class="fill-white">
                         <path d="M777.61-140q-113.07 0-227.19-52.58-114.11-52.58-209.69-148.34-95.57-95.77-148.15-209.7Q140-664.54 140-777.61q0-18.17 12-30.28T182-820h130.46q15.15 0 26.73 9.89 11.58 9.88 14.73 24.42L376.85-668q2.38 16.38-1 28.15-3.39 11.77-12.16 19.77l-92.38 89.93q22.3 40.84 50.96 77.27 28.65 36.42 62.04 69.57 32.92 32.93 70 61.16 37.08 28.23 80.08 52.54l89.76-90.54q9.39-9.77 22.74-13.7 13.34-3.92 27.73-1.92l111.07 22.62q15.15 4 24.73 15.46t9.58 26V-182q0 18-12.11 30t-30.28 12ZM242.92-586.92l71.39-68.31q1.92-1.54 2.5-4.23.58-2.69-.19-5l-17.39-89.39q-.77-3.07-2.69-4.61-1.92-1.54-5-1.54H206q-2.31 0-3.85 1.54-1.53 1.54-1.53 3.85 3.07 41 13.42 83.3 10.34 42.31 28.88 84.39Zm348 345.69q39.77 18.54 82.96 28.35 43.2 9.8 80.73 11.65 2.31 0 3.85-1.54t1.54-3.85v-84.15q0-3.08-1.54-5t-4.61-2.69l-84-17.08q-2.31-.77-4.04-.19-1.73.58-3.66 2.5l-71.23 72Zm-348-345.69Zm348 345.69Z"/>
                     </svg> +88 01833156010</a>
-            </div>
-            <div class="flex lg:w-6/12 w-full fadeIn animated" data--delay=".4s">
-                <?php echo do_shortcode('[jet_fb_form form_id="57" submit_type="ajax" required_mark="*" fields_layout="column" fields_label_tag="label" markup_type="fieldset" enable_progress="" clear="1"]'); ?>
             </div>
         </div>
     </div>
