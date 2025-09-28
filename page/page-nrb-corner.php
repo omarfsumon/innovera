4e73ebf09c28106fd70a6bb1dff3e44480e34ae6
omarfsumon <hmfarukhossain96@gmail.com>
2025-09-28 09:03:04 +0600

    start NRB Corner

---
+++ page/page-nrb-corner.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<?php get_template_part('template-parts/banner', 'nrb-corner'); ?>
+<section class="nrb-corner my-10">
+    <div class="container ">
+        <h6 class="italic text-4th-heading text-center">No matter where you live, your home and land in Bangladesh are safe with us.</h6>
+    </div>
+</section>
+
+
+
+
+
+<?php get_footer(); ?>
