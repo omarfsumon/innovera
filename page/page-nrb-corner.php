35b13624041825d027d98a3dc4ebc0adff1b5015
omarfsumon <hmfarukhossain96@gmail.com>
2025-09-28 19:25:22 +0600

    NRB Corner page 50%

--- page/page-nrb-corner.php
+++ page/page-nrb-corner.php
@@ -1,13 +1,91 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/banner', 'nrb-corner'); ?>
-<section class="nrb-corner my-10">
+<section class="pb-5 pt-10">
     <div class="container ">
-        <h6 class="italic text-4th-heading text-center">No matter where you live, your home and land in Bangladesh are safe with us.</h6>
+        <h6 class="italic text-4th-heading text-center" data-aos="flip-up" data-aos-delay="100">No matter where you live, your home and land in Bangladesh are safe with us.</h6>
+    </div>
+</section>
+
+<section class="py-10">
+    <div class="container">
+        <div class="flex flex-col md:flex-row items-center justify-between gap-5">
+            <div class="max-w-[100%] md:max-w-[35%] md:order-last">
+                <img src="<?php echo get_template_directory_uri(); ?>/src/images/nrb/nrb_corner.webp" alt="NRB Corner" class="w-full rounded-lg" data-aos="fade-left" data-aos-delay="400">
+            </div>
+            <div class="max-w-[100%] md:max-w-[65%]" data-aos="fade-right" data-aos-delay="100">
+                <h2 class="text-3rd-heading font-semibold">For Our NRB Customers & Landowners</h2>
+                <h3 class="text-xl font-semibold">Your Trusted Real Estate Partner in Bangladesh</h3>
+                <p data-aos="fade-right">At <strong>Innovera Holdings Ltd.</strong>, we deeply value the trust of our <strong>Non-Resident Bangladeshi (NRB) clients</strong> who dream of owning a home or developing their land back in Bangladesh. 
+                    Whether you are working abroad or settled overseas, we ensure that your investment is <strong>safe, secure, and rewarding.</strong></p>
+            </div>
+        </div>
+        <div class="my-10">
+            <h4 class="text-2xl font-semibold text-center">For NRB Homebuyers</h4>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-[3/2]" 
+                    data-aos="zoom-in" data-aos-duration="1000">
+                    <h4 class="text-2xl leading-[1.2]">Hassle-Free Apartment Purchase</h4>
+                    <p>Book, pay, and complete all documentation from abroad.</p>
+                </div>
+                <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-[3/2]" 
+                    data-aos="zoom-in" data-aos-duration="1000">
+                    <h4 class="leading-[1.2] text-2xl">Legal & Documentation Support</h4>
+                    <p>We assist with POA (Power of Attorney), registration, and all approvals.</p>
+                </div>
+                <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-[3/2]" 
+                    data-aos="zoom-in" data-aos-duration="1000">
+                    <h4 class="text-2xl leading-[1.2]">Flexible Payment Plans</h4>
+                    <p>Easy installment options through banking channels.</p>
+                </div>
+                <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-[3/2]" 
+                    data-aos="zoom-in" data-aos-duration="1000">
+                    <h4 class="text-2xl leading-[1.2]">Customization Options</h4>
+                    <p>Choose your preferred interiors, finishes, and fittings—even from overseas.</p>
+                </div>
+                <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-[3/2]" 
+                    data-aos="zoom-in" data-aos-duration="1000">
+                    <h4 class="text-2xl leading-[1.2]">Property Management Service</h4>
+                    <p>We maintain your apartment until you or your family are ready to move in.</p>
+                </div>
+                <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-[3/2]" 
+                    data-aos="zoom-in" data-aos-duration="1000">
+                    <h4 class="text-2xl leading-[1.2]">High-Value Investment</h4>
+                    <p>Buy in prime locations with strong rental and resale demand.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
 
 
 
-
+<section class="md:py-16 py-10 bg-white">
+    <div class="container">
+        <h2 class="text-3rd-heading leading-[1.2] mb-2 font-[400] text-center" data-aos="flip-up" data-aos-duration="1000">
+        Why NRBs Choose Innovera</h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 mt-5">
+            <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-square" data-aos="zoom-in" data-aos-duration="1000">
+                <?php get_template_part('src/svg/check', 'check'); ?>
+                <h4 class="text-xl leading-[1.2]">100% Transparency in all dealings</h4>
+            </div>
+            <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-square" data-aos="zoom-in" data-aos-duration="1000">
+                <?php get_template_part('src/svg/check', 'check'); ?>
+                <h4 class="leading-[1.2] text-xl">Strong legal and financial safeguards</h4>
+            </div>
+            <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-square" data-aos="zoom-in" data-aos-duration="1000">
+                <?php get_template_part('src/svg/check', 'check'); ?>
+                <h4 class="text-xl leading-[1.2]">Digital updates, progress reports, and video calls during construction</h4>
+            </div>
+            <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-square" data-aos="zoom-in" data-aos-duration="1000">
+                <?php get_template_part('src/svg/check', 'check'); ?>
+                <h4 class="text-xl leading-[1.2]">After-sales support & long-term relationship</h4>
+            </div>
+            <div class="flex flex-col p-5 rounded-lg gap-2 shadow justify-center items-center text-center hover:shadow-lg transition duration-300 aspect-square" data-aos="zoom-in" data-aos-duration="1000">
+                <?php get_template_part('src/svg/check', 'check'); ?>
+                <h4 class="text-xl leading-[1.2]">A trusted name in Bangladesh</h4>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
