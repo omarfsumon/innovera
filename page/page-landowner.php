cd6cfd730c9bc71c0a7ad3bee7a93efa15de62cc
omarfsumon <hmfarukhossain96@gmail.com>
2025-10-20 09:08:40 +0600

    call to action section

--- page/page-landowner.php
+++ page/page-landowner.php
@@ -141,4 +141,44 @@
         </div>
     </div>
 </section>
+
+<section class="md:py-16 py-10 bg-white">
+    <div class="container text-center">
+        <h2 class="text-2nd-heading leading-[1.2] mb-2 font-[400] scroll_animate" data-animate="animate__flipInX">Ready to Unlock Your Land's Potential?</h2>
+        <p class="text-base leading-[1.5] max-w-2xl mx-auto mt-4 scroll_animate" data-animate="animate__fadeInUp">Partner with Innovera for a seamless joint venture experience that maximizes your land's value while minimizing your risks. Contact us today to explore the possibilities!</p>
+        <div class="flex flex-col md:flex-row justify-center items-center gap-5 mt-6">
+            <a class="flex items-center  gap-2 text-lg font-medium px-3 py-2 mt-4 rounded border border-dashed border-secondary-100 scroll_animate" href="tel:+8801833156010" data-animate="animate__fadeIn">
+                <div class="flex items-center justify-center p-3 rounded-full bg-secondary-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="28px" width="28px" viewBox="0 -960 960 960" class="fill-secondary-500">
+                        <path d="M777.61-140q-113.07 0-227.19-52.58-114.11-52.58-209.69-148.34-95.57-95.77-148.15-209.7Q140-664.54 140-777.61q0-18.17 12-30.28T182-820h130.46q15.15 0 26.73 9.89 11.58 9.88 14.73 24.42L376.85-668q2.38 16.38-1 28.15-3.39 11.77-12.16 19.77l-92.38 89.93q22.3 40.84 50.96 77.27 28.65 36.42 62.04 69.57 32.92 32.93 70 61.16 37.08 28.23 80.08 52.54l89.76-90.54q9.39-9.77 22.74-13.7 13.34-3.92 27.73-1.92l111.07 22.62q15.15 4 24.73 15.46t9.58 26V-182q0 18-12.11 30t-30.28 12ZM242.92-586.92l71.39-68.31q1.92-1.54 2.5-4.23.58-2.69-.19-5l-17.39-89.39q-.77-3.07-2.69-4.61-1.92-1.54-5-1.54H206q-2.31 0-3.85 1.54-1.53 1.54-1.53 3.85 3.07 41 13.42 83.3 10.34 42.31 28.88 84.39Zm348 345.69q39.77 18.54 82.96 28.35 43.2 9.8 80.73 11.65 2.31 0 3.85-1.54t1.54-3.85v-84.15q0-3.08-1.54-5t-4.61-2.69l-84-17.08q-2.31-.77-4.04-.19-1.73.58-3.66 2.5l-71.23 72Zm-348-345.69Zm348 345.69Z"/>
+                    </svg>
+                </div>
+                <div class="flex flex-col">
+                    <h6 class="text-lg text-left font-normal md:font-medium leading-relaxed">Call now to book a meeting!</h6>
+                    <p class="lg:text-lg text-base text-left font-normal text-black/50">+88 01833156010</p>
+                </div>
+            </a>
+            <a class="flex items-center  gap-2 text-lg font-medium px-3 py-2 mt-4 rounded border border-dashed border-secondary-100 scroll_animate" href="tel:+8802226617206" data-animate="animate__fadeIn">
+                <div class="flex items-center justify-center p-3 rounded-full bg-secondary-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="28px" width="28px" viewBox="0 -960 960 960" class="fill-secondary-500">
+                        <path d="M682.69-197.69h59.23q5.39 0 8.85-3.46 3.46-3.47 3.46-8.85v-535.38q0-5.39-3.46-8.85t-8.85-3.46h-59.23q-5.38 0-8.84 3.46-3.47 3.46-3.47 8.85V-210q0 5.38 3.47 8.85 3.46 3.46 8.84 3.46Zm-394.63-207.7q14.71 0 24.67-9.94 9.96-9.95 9.96-24.66 0-14.7-9.95-24.66-9.94-9.96-24.65-9.96-14.7 0-24.67 9.94-9.96 9.95-9.96 24.66 0 14.7 9.95 24.66t24.65 9.96Zm0 120q14.71 0 24.67-9.94 9.96-9.95 9.96-24.66 0-14.7-9.95-24.66-9.94-9.96-24.65-9.96-14.7 0-24.67 9.94-9.96 9.95-9.96 24.66 0 14.7 9.95 24.66t24.65 9.96ZM253.46-530h309.23v-140H253.46v140Zm154.6 124.61q14.71 0 24.67-9.94 9.96-9.95 9.96-24.66 0-14.7-9.95-24.66-9.94-9.96-24.65-9.96-14.7 0-24.67 9.94-9.96 9.95-9.96 24.66 0 14.7 9.95 24.66t24.65 9.96Zm0 120q14.71 0 24.67-9.94 9.96-9.95 9.96-24.66 0-14.7-9.95-24.66-9.94-9.96-24.65-9.96-14.7 0-24.67 9.94-9.96 9.95-9.96 24.66 0 14.7 9.95 24.66t24.65 9.96Zm120-120q14.71 0 24.67-9.94 9.96-9.95 9.96-24.66 0-14.7-9.95-24.66-9.94-9.96-24.65-9.96-14.7 0-24.67 9.94-9.96 9.95-9.96 24.66 0 14.7 9.95 24.66t24.65 9.96Zm0 120q14.71 0 24.67-9.94 9.96-9.95 9.96-24.66 0-14.7-9.95-24.66-9.94-9.96-24.65-9.96-14.7 0-24.67 9.94-9.96 9.95-9.96 24.66 0 14.7 9.95 24.66t24.65 9.96Zm82.33 47.7v-480H218.08q-5.39 0-8.85 3.46t-3.46 8.85V-250q0 5.38 3.46 8.85 3.46 3.46 8.85 3.46h392.31Zm72.3 100q-21.93 0-38.62-11.2-16.68-11.19-25.76-28.8H218.08q-29.83 0-51.07-21.24-21.24-21.24-21.24-51.07v-455.38q0-29.83 21.24-51.07 21.24-21.24 51.07-21.24h400.23q9.08-17.62 25.76-28.81 16.69-11.19 38.62-11.19h59.23q29.83 0 51.07 21.24 21.24 21.24 21.24 51.07V-210q0 29.83-21.24 51.07-21.24 21.24-51.07 21.24h-59.23Zm-476.92-100v-480 480Z"/>
+                    </svg>
+                </div>
+                <div class="flex flex-col">
+                    <h6 class="text-left">BTCL</h6>
+                    <p class="lg:text-lg text-left text-base font-normal text-black/50">+88 02226617206</p>
+                </div>
+            </a>
+            <a href="mailto:info@innovera.com.bd">
+                <div class="flex justify-center mt-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#e8b061"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-1 14H5c-.55 0-1-.45-1-1V8l6.94 4.34c.65.41 1.47.41 2.12 0L20 8v9c0 .55-.45 1-1 1zm-7-7L4 6h16l-8 5z"/></svg>
+                    <div class="ms-3">
+                        <h6 class="text-left">Email Us</h6>
+                        <p class="lg:text-lg text-left text-base font-normal text-black/50">info@innovera.com.bd</p>
+                    </div>
+                </div>  
+            </a>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
